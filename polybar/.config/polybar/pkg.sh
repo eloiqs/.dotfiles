37040ebd4f260f522348702a58aa76395c9d150c
eloi simard-quesnel <eloi.qs@gmail.com>
2017-09-21 18:31:52 -0400

    big clean up for clean arch install

--- polybar/.config/polybar/pkg.sh
+++ polybar/.config/polybar/pkg.sh
@@ -5,8 +5,8 @@ totalAur=$(packer -Su | wc -l)
 check=$((totalPac + totalAur))
 if [[ "$check" != "0" ]]
 then
-    pac="%{A:termite -e 'yaourt -Syu' &:}$totalPac%{A}"
+    pac="%{A:termite -e 'packer -Syu' &:}$totalPac%{A}"
     icon="%{F#ec5f67}%{F-}"
-    aur="%{A:termite -e 'yaourt -Syu --aur' &:}$totalAur%{A}"
+    aur="%{A:termite -e 'packer -Syu --aur' &:}$totalAur%{A}"
     echo "$pac $icon $aur"
 fi
